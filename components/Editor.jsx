5ed8ae9990b442660078fb81d284f8bf2db6e7d2
TZ113 <earnestlearneran@gmail.com>
2023-12-04 22:24:18 +0600

    updated the Editor component

--- components/Editor.jsx
+++ components/Editor.jsx
@@ -1,21 +1,20 @@
-import PropTypes from "prop-types"
-import React from "react"
-import ReactMde from "react-mde"
-import Showdown from "showdown"
+import PropTypes from "prop-types";
+import React from "react";
+import ReactMde from "react-mde";
+import 'react-mde/lib/styles/css/react-mde-all.css';
+import Showdown from "showdown";
+import xssfilter from "showdown-xss-filter";
 
 // Component representing the Editor for note text
 // It takes two props: tempNoteText, updateTempNoteText
 export default function Editor({ tempNoteText, updateTempNoteText }) {
     // State to manage the selected tab (write/preview) in the editor
-    const [selectedTab, setSelectedTab] = React.useState("write")
+    const [selectedTab, setSelectedTab] = React.useState("write" | "preview")
 
     // Create a showdown converter with specific options for markdown rendering
-    const converter = new Showdown.Converter({
-        tables: true,
-        simplifiedAutoLink: true,
-        strikethrough: true,
-        tasklists: true,
-    })
+    const converter = new Showdown.Converter({ extensions: [xssfilter] })
+    converter.setFlavor('github')
+
 
     // Render the Editor component with ReactMde, providing necessary props
     return (
@@ -29,6 +28,7 @@ export default function Editor({ tempNoteText, updateTempNoteText }) {
                     Promise.resolve(converter.makeHtml(markdown))
                 }
                 minEditorHeight={80}
+                maxEditorHeight={100}
                 heightUnits="vh"
             />
         </section>
